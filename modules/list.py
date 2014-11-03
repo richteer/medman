555226daa0f000fd7a1243de3888f2392bb98c84
richteer <richteer@lastprime.net>
2014-11-03 11:51:43 -0500

    List now takes in a name argument

--- modules/list.py
+++ modules/list.py
@@ -2,11 +2,25 @@ import os,json
 
 def medman_list(config, commands, args):
 	# TODO: Error-check that config has a base registration path
-	with open(config["path"].replace("~",os.environ["HOME"]) + "/index.json","r") as f: # TODO: Clean up the stupid $HOME issue
+
+
+	# Default action: Display all registered names
+	if len(args) == 0:
+		with open(config["path"].replace("~",os.environ["HOME"]) + "/index.json","r") as f: # TODO: Clean up the stupid $HOME issue
+			ls = json.loads(f.read())["media"]
+
+		for l in ls:
+			print(l["name"])
+
+		return
+
+	# Otherwise, find the name specified, and display its sublist
+	with open(config["path"].replace("~", os.environ["HOME"]) + "/index.json", "r") as f:
 		ls = json.loads(f.read())["media"]
 
-	
-
-	
 	for l in ls:
-		print(l["name"])
+		if l["name"] == args[0]:
+			print(l["path"]) # TODO: Replace with files known to this
+			return
+
+	print("Title '{}' not registered!".format(args[0]))
